b78c603686cdd7895c33c21c206f9b1cd3cfcb6f
Atilla GÜNTÜRK <agunturk65@yahoo.com>
2025-07-13 13:20:41 +0300

    Update MainActivity.java

--- app/src/main/Java/com/Atilla/geziharitam/MainActivity.java
+++ app/src/main/Java/com/Atilla/geziharitam/MainActivity.java
@@ -1,1 +1,21 @@
-java\npackage com.atilla.geziharitam;\nimport android.os.Bundle;\nimport android.webkit.WebSettings;\nimport android.webkit.WebView;\nimport androidx.appcompat.app.AppCompatActivity;\npublic class MainActivity extends AppCompatActivity {\n  @Override protected void onCreate(Bundle savedInstanceState) {\n    super.onCreate(savedInstanceState);\n    WebView wv = new WebView(this);\n    setContentView(wv);\n    WebSettings s = wv.getSettings();\n    s.setJavaScriptEnabled(true);\n    s.setAllowFileAccess(true);\n    s.setDomStorageEnabled(true);   // IndexedDB\n    wv.loadUrl(\"file:///android_asset/index.html\");\n  }\n}\n
+package com.atilla.geziharitam;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    WebView wv = new WebView(this);          // Yeni WebView oluştur
+    setContentView(wv);                      // WebView'i aktivitenin görünümü yap
+    WebSettings s = wv.getSettings();       // WebView ayarlarını al
+    s.setJavaScriptEnabled(true);            // JavaScript çalıştırmaya izin ver
+    s.setAllowFileAccess(true);              // Dosya erişimini aç
+    s.setDomStorageEnabled(true);            // IndexedDB gibi DOM depolama izin ver
+    wv.loadUrl("file:///android_asset/index.html");  // Assets klasöründeki index.html dosyasını yükle
+  }
+}
