595b83880db58f30628030b1725b9db8e25bcaef
Atilla GÜNTÜRK <agunturk65@yahoo.com>
2025-08-30 14:41:26 +0300

    Update ilSec.js

--- app/src/main/assets/ilSec.js
+++ app/src/main/assets/ilSec.js
@@ -1,16 +1,8 @@
 /* ---------- ilSec.js ---------- */
 // İl seçimi ve haritada o ile gitme
 
-// İl listesi örnek (sen kendi koordinatları ile güncelle)
-const iller = [
-  { isim: "Türkiye", koordinat: [39.0, 35.0] },
-  { isim: "Adana", koordinat: [37.0, 35.3] },
-  { isim: "Adıyaman", koordinat: [37.8, 38.3] },
-  { isim: "Afyonkarahisar", koordinat: [38.7, 30.5] },
-  // ... diğer iller
-];
-
 const ilSelect = document.getElementById("ilSec");
+
 iller.forEach((il, i) => {
   const op = document.createElement("option");
   op.value = i; 
