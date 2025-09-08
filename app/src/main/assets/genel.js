f96bbdaedbfe458fb273a2c086a3cd8e8f67d24b
Atilla GÜNTÜRK <agunturk65@yahoo.com>
2025-09-08 18:29:47 +0300

    Update genel.js

--- app/src/main/assets/genel.js
+++ app/src/main/assets/genel.js
@@ -100,14 +100,14 @@ function markerSil(i) {
   if (window.markerlar && window.markerlar[i]) {
     window.harita.removeLayer(window.markerlar[i]);
     window.markerlar.splice(i, 1);
-  }
+  
 
   // Veri kaldır
   window.veriler.splice(i, 1);
 
   // Paneli tamamen gizle
   document.getElementById("bilgiPaneli").innerHTML = "🗺️ Haritadan bir yeri seçtiğinizde detayları burada görünecek";
-
+ }
   // Harita görünümünü varsayılana döndür
   if (window.harita) window.harita.setView([39.0, 35.0], 6);
 
